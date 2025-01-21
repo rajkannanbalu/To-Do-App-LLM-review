7b25926fcf6cc6467aad509cbbb51e2f31efb682
b.rajkannan <b.rajkannan@gmail.com>
2025-01-21 18:58:56 +0530

    test

--- User/usecase/user_usecase.go
+++ User/usecase/user_usecase.go
@@ -1,36 +1,36 @@
 package usecase
 
 import (
-
 	"context"
+	"fmt"
 	"time"
-	
-	"To_Do_App/models"
-	"To_Do_App/User"
 
+	"To_Do_App/User"
+	"To_Do_App/models"
 )
 
 type userUsecase struct {
-	userRepo 		User.Repository
-	contextTimeout  time.Duration
+	userRepo       User.Repository
+	contextTimeout time.Duration
 }
 
 func NewUserUsecase(u User.Repository, timeout time.Duration) User.Usecase {
-	
-	return &userUsecase {
-		userRepo: 		u,
+
+	return &userUsecase{
+		userRepo:       u,
 		contextTimeout: timeout,
 	}
 }
 
 // Store new data in the database
-func (u *userUsecase) Store(c context.Context, user *models.UserDB) error{
-	
+func (u *userUsecase) StoreV1(c context.Context, user *models.UserDB) error {
+
 	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
 	defer cancel()
 
-	err := u.userRepo.Store(ctx, user)
-	if err != nil{
+	fmt.Println("test")
+	err := u.userRepo.StoreV1(ctx, user)
+	if err != nil {
 		return err
 	}
 
@@ -38,7 +38,7 @@ func (u *userUsecase) Store(c context.Context, user *models.UserDB) error{
 }
 
 // Update user info
-func (u *userUsecase) Update(c context.Context, user *models.UserDB) error{
+func (u *userUsecase) Update(c context.Context, user *models.UserDB) error {
 
 	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
 	defer cancel()
@@ -47,14 +47,14 @@ func (u *userUsecase) Update(c context.Context, user *models.UserDB) error{
 
 }
 
-// fetch all the task in the task database 
-func (u *userUsecase) GetAllUser(c context.Context) ([]*models.UserDB, error){
-	 
+// fetch all the task in the task database
+func (u *userUsecase) GetAllUser(c context.Context) ([]*models.UserDB, error) {
+
 	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
 	defer cancel()
-	
+
 	res, err := u.userRepo.GetAllUser(ctx)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
