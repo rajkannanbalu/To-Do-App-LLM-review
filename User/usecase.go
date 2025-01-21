ad097e7bfee3bad32cee52cbbcfb7f1297eb4b3e
b.rajkannan <b.rajkannan@gmail.com>
2025-01-21 18:26:46 +0530

    github action test

--- User/usecase.go
+++ User/usecase.go
@@ -1,19 +1,12 @@
 package User
 
 import (
-
-	"context"
 	"To_Do_App/models"
-	
+	"context"
 )
 
-
-
-
 type Usecase interface {
-
-	Store(ctx context.Context, a *models.UserDB) error
-	Update(ctx context.Context, a *models.UserDB) error	
+	StoreV1(ctx context.Context, a *models.UserDB) error
+	Update(ctx context.Context, a *models.UserDB) error
 	GetAllUser(c context.Context) ([]*models.UserDB, error)
-
 }
